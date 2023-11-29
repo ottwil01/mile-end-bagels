132d254d3ebdc414d602e85104a4195e86c7f392
ottwil01 <ottwil01@gmail.com>
2023-11-29 16:04:14 +1100

    colmak-dh

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,106 +1,102 @@
-import menuData from '../assets/menu.json';
+import MenuData from '../assets/menu.json';
 import { useState, useEffect } from 'react';
 
 
 export default function Menu() {
-  const sandwiches = menuData.menu.find(
-    (category) => category.header === 'Sandwiches'
-  );
-  const otherMenus = menuData.menu.filter(
-    (category) => category.header !== 'Sandwiches'
-  );
 
-  const allItems = [];
+  // Initialize strike-through status for all items
+  const initialStrikethroughStatus = Object.keys(MenuData).reduce((acc, category) => {
+    acc[category] = MenuData[category].map(() => false);
+    return acc;
+  }, {});
 
-// Iterate through the menu sections
-menuData.menu.forEach(menuSection => {
-  // Iterate through the items in each section
-  menuSection.items.forEach(item => {
-    // Add the item to the allItems array
-    allItems.push(item);
-  });
-});
-  console.log(allItems)
-
-// sandwiches toggle
-  const [strikethroughStatus, setStrikethroughStatus] = useState(
-    sandwiches.items.map(() => false)
-  );
+  // State for strike-through status
+  const [strikethroughStatus, setStrikethroughStatus] = useState(initialStrikethroughStatus);
 
   // Load the strike-through status from local storage when the component mounts
   useEffect(() => {
-    const storedStatus = localStorage.getItem('strikethroughStatus');
-    if (storedStatus) {
-      setStrikethroughStatus(JSON.parse(storedStatus));
+    const storedStrikethroughStatus = localStorage.getItem('strikethroughStatus');
+    if (storedStrikethroughStatus) {
+      setStrikethroughStatus(JSON.parse(storedStrikethroughStatus));
     }
   }, []);
 
-  const toggleStrikethrough = (index) => {
-    const updatedStatus = [...strikethroughStatus];
-    updatedStatus[index] = !updatedStatus[index];
+  const toggleStrikethrough = (category, index) => {
+    const updatedStatus = { ...strikethroughStatus };
+    updatedStatus[category][index] = !updatedStatus[category][index];
     setStrikethroughStatus(updatedStatus);
     // Save the updated status to local storage
     localStorage.setItem('strikethroughStatus', JSON.stringify(updatedStatus));
   };
 
-  const resetStrikethrough = () => {
-    setStrikethroughStatus(sandwiches.items.map(() => false));
+  const resetStrikethrough = (category) => {
+    const updatedStatus = { ...strikethroughStatus };
+    updatedStatus[category] = updatedStatus[category].map(() => false);
+    setStrikethroughStatus(updatedStatus);
     // Reset the status in local storage
     localStorage.removeItem('strikethroughStatus');
   };
-// othermenus toggle
-  // const [otherStatus, setOtherStatus] = useState(otherMenus.items.map(() => false))
-
-  //   useEffect(() => {
-  //   const storedStatus = localStorage.getItem('otherStatus')
-  //   if (storedStatus) {
-  //     setOtherStatus(JSON.parse(storedStatus))
-  //   }
-  // }, []);
-
-  // const toggleOtherstrikethrough = (index) => {
-  //   const updatedStatus = [...otherStatus]
-  //   updatedStatus[index] = !updatedStatus[index]
-  //   setOtherStatus(updatedStatus)
-  //   localStorage.setItem('otherStatus', JSON.stringify(updatedStatus))
-  // }
-
-  // const resetOther = () => {
-  //   setOtherStatus(otherMenus.items.map(() => false))
-  //   localStorage.removeItem('otherStatus')
-  // }
-
 
   return (
-    <div className="flex flex-row gap-20 m-10">
-      <div className="flex flex-col">
-        <h1 className="text-3xl ml-7">{sandwiches.header}</h1>
-        <button onClick={resetStrikethrough}>Reset</button>
-        <div className="relative leading-normal">
-          {sandwiches.items.map((item, i) => (
-            <div
-              key={i}
-              className={`flex text-lg w-full ${strikethroughStatus[i] ? 'line-through' : ''}`}
-              onClick={() => toggleStrikethrough(i)}
-            >
-              <span className="w-[30px]">{i + 1}</span>
-              <span className="mr-5">{item.name}</span>
-              <span>{item.price}</span>
-            </div>
-          ))}
+    <div>
+      <div className="flex flex-row gap-20 m-10 bg-[url(./outline.svg)]">
+        <div className="flex flex-col">
+          <h1 className="text-3xl ml-7">Sandwiches</h1>
+          {/* <button onClick={() => resetStrikethrough('sandwiches')}>Reset</button> */}
+          <div className="relative leading-normal">
+            {MenuData.sandwiches.map((item, i) => (
+              <div
+                key={i}
+                className="flex text-lg w-full"
+              /* onClick={() => toggleStrikethrough('sandwiches', i)} */
+              >
+                <span className="w-[30px]">{i + 1}</span>
+                <span className="mr-5">{item.name}</span>
+                <span>{item.price}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div>
+            <Header text="Bagels" />
+            <Items items={MenuData.bagels} />
+          </div>
+          <div>
+            <Header text="Hot Drinks" />
+            <Items items={MenuData.hotDrinks} />
+          </div>
+          <div>
+            <Header text="Cold Drinks" />
+            <Items items={MenuData.coldDrinks} />
+          </div>
         </div>
       </div>
-      <div>
-        {otherMenus.map((category, i) => (
-          <div key={i}>
-            <h1 className="text-3xl">{category.header}</h1>
-            {/* <button onClick={resetOther}>Reset</button> */}
+    </div>
+  );
+}
+
+function Header(props) {
+
+  return (
+    <h1 className="text-3xl">{props.text}</h1>
+  )
+}
+
+ function Items(props) {
+
+   const items = props.items
+
+   return (
+          <div className="mb-4">
+            {/* <button onClick={() => resetStrikethrough('otherMenus', i)}>Reset</button> */}
             <div className="relative leading-normal">
-              {category.items.map((item, i) => (
+              {items.map((item, i) => (
                 <div
                   key={i}
                   className="flex text-lg w-full"
-                  /* onClick={() => toggleOtherstrikethrough(i)} */
+                  /* className={`flex text-lg w-full ${otherMenuStrikethroughStatus[i][j] ? 'line-through' : ''}`} */
+                  /* onClick={() => toggleStrikethrough('otherMenus', j)} */
                 >
                   <span className="mr-5">{item.name}</span>
                   <span>{item.price}</span>
@@ -108,8 +104,5 @@ menuData.menu.forEach(menuSection => {
               ))}
             </div>
           </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+   )
+ }
